2a67f4ba747f1bf9c9004126b740e1daa1a9716c
mostlytricks <trabolkia@naver.com>
2022-05-22 03:33:18 +0900

    finish follow up

---
+++ js/todolist.js
@@ -0,0 +1,64 @@
+const todoForm = document.querySelector(".todo-form");
+const todoList = document.querySelector(".todo-list");
+const todoInput = document.querySelector(".todo-form input");
+
+let toDos = []; /* temp storage */
+
+const TODOS_KEY = "todos";
+
+const saveToDo = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  console.log(toDos);
+};
+
+const deleteToDo = (e) => {
+  const li = e.target.parentNode;
+  console.log(li);
+  toDos = toDos.filter((toDo) => {
+    // 필터 사용 유의, type 유의할 것
+    toDo.id !== parseInt(li.id);
+  });
+  li.remove();
+  saveToDo();
+};
+
+const paintToDo = (newToDoObj) => {
+  if (newToDoObj === "") {
+    return -1;
+  }
+  const line = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  line.id = newToDoObj.id;
+  button.innerText = "🆇";
+  button.addEventListener("click", deleteToDo);
+  span.innerText = newToDoObj.text;
+
+  line.appendChild(span);
+  line.appendChild(button);
+  todoList.appendChild(line);
+};
+
+const toDoSubmit = (e) => {
+  e.preventDefault();
+  const newToDo = todoInput.value;
+  todoInput.value = "";
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDo();
+};
+
+todoForm.addEventListener("submit", toDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  console.log(parsedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
---
+++ js/weather.js
@@ -0,0 +1,28 @@
+const onGeoLoaded = (position) => {
+  console.log(position);
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY_FOR_WUT}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      //앞에서 리턴해서 파싱하는것 유의해두자.
+      console.log(data);
+
+      const weatherTag = document.querySelector(".weather span:first-child");
+      const cityTag = document.querySelector(".weather span:last-child");
+      cityTag.innerText = `${data.name}`;
+      weatherTag.innerText = data.weather[0].main;
+    });
+};
+
+const onGeoError = (e) => {
+  alert("Can't determine the location");
+};
+
+navigator.geolocation.getCurrentPosition(onGeoLoaded, onGeoError);
+
+const KEY_FOR_WUT = "23b76978f6187a38f3b63c93b5797e9b";
