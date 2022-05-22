d3ce2e5853631576823ba60da3a518b4b03a82de
mostlytricks <trabolkia@naver.com>
2022-05-22 09:47:40 +0900

    test background

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -6,5 +6,5 @@ const bgImage = document.querySelector(".bg img.bgImage");
 const pageName = "mostlytricks.github.io/";
 
 // bgImage.src = `images/${chosenImage}`;
-bgImage.style.backgroundImage = "url('pageName/../images/darkmode.jpg')";
+bgImage.style.backgroundImage = "url('`${pageName}`/../images/darkmode.jpg')";
 console.log(bgImage.style.backgroundImage);
